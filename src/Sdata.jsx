0ec136e21f9523ebe6815d86bbf0f094ec7c0174
Shakil000 <racer.shakil111@gmail.com>
2021-01-22 03:38:33 +0600

    first commit

---
+++ src/Sdata.jsx
@@ -0,0 +1,33 @@
+const Sdata=[
+    {
+        sname: "DARK",
+        imgsrc: "https://wallpapercave.com/wp/wp4056410.jpg",
+        title: "Netflix Original Serise",
+        links: "https://www.netflix.com/bd/title/80100172",
+    },
+    {
+        sname: "Extra Curricular",
+        imgsrc: "https://www.hitc.com/static/uploads/hitcn/1816/extracurricular_netflix_780_1544944.jpg",
+        title: "Netflix Original Serise",
+        links: "https://www.netflix.com/in/title/80990668?source=35",
+    },
+    {
+        sname: "Stranger Things",
+        imgsrc: "https://www.pyramidinternational.com/assets/cdn/brands/STARNGER-THINGS-PAGE-BANNER-IMAGE_01.jpg-4fb0fa7eb67e16b356e1d53247ff6e60.jpg",
+        title: "Netflix Original Serise",
+        links: "https://www.netflix.com/bd/title/80057281",
+    },
+    {
+        sname: "The Vampire Diaries",
+        imgsrc: "https://occ-0-990-987.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABaXYmzxE2zEeSYR-peqFjrLH5ZOe_YGeaJI3L-_DQ8SfVfRoobFb6IIN3QvTSugcy-MOdFpCcklCsThL5vBCOW9oQQEO.jpg?r=725",
+        title: "Netflix Original Serise",
+        links: "https://www.imdb.com/title/tt1405406/?ref_=ttep_ep_tt",
+    },
+    {
+        sname: "My First-2 Love",
+        imgsrc: "https://i.pinimg.com/474x/7b/19/58/7b19584ec5ffba063a121508e78fe7a4.jpg",
+        title: "Netflix Original Serise",
+        links: "https://www.netflix.com/bd/title/81026700",
+    },
+];
+export default Sdata;
